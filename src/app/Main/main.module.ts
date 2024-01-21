e1e7535a96315bfac754da9132a8fb1c5cf59f9f
Doha Gamal Elgabry <118745582+Dohaelgabry@users.noreply.github.com>
2024-01-21 21:46:35 +0200

    login_page

--- src/app/Main/main.module.ts
+++ src/app/Main/main.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainPageComponent } from './main-page/main.component';
+import { LoginComponent } from './login/login/login.component';
+import { MatIconModule } from '@angular/material/icon';
+
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
-  exports: [MainPageComponent]
+  exports: [MainPageComponent, LoginComponent]
 })
 export class MainPageModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageModule } from './Main/main.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,11 @@ import { MainPageModule } from './Main/main.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainPageModule
+    MainPageModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
