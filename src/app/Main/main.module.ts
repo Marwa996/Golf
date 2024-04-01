005d61f425b61ba8e555d3fbba5edc05ba4246bc
Marwa996 <marwamoneim221@gmail.com>
2024-04-01 16:55:50 +0200

    allowing theme changing with toggle

--- src/app/Main/main.module.ts
+++ src/app/Main/main.module.ts
@@ -1,15 +1,16 @@
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { RouterOutlet } from '@angular/router';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     MainPageComponent,
@@ -18,13 +19,13 @@ import {CdkAccordionModule} from '@angular/cdk/accordion';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    CdkAccordionModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   exports: [SidenavComponent]
 })
--- src/app/Main/sidenav/sidenav.component.ts
+++ src/app/Main/sidenav/sidenav.component.ts
@@ -1,47 +1,66 @@
-import { Component, computed, signal } from "@angular/core";
-
+import { Component, HostBinding, OnInit, computed, signal } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { OverlayContainer } from '@angular/cdk/overlay'
 @Component({
-    selector:'app-sidenav',
-    templateUrl:'./sidenav.component.html',
-    styleUrls:['./sidenav.component.scss']
+    selector: 'app-sidenav',
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
+    @HostBinding('class') className = ''
+    darkClass = 'theme-dark';
+    lightClass = 'theme-light'
+    switchTheme = new FormControl(false)
+
+    constructor(private overlay: OverlayContainer) { }
 
     menuItems = signal<MenuItem[]>([
         {
-            icon:'dashboard',
+            icon: 'dashboard',
             label: 'Main',
             route: 'main',
-            categories:['products','prices']
+            categories: ['products', 'prices']
 
         },
         {
-            icon:'add_shopping_cart',
+            icon: 'add_shopping_cart',
             label: 'Products',
             route: 'products',
-            categories:['users','clients']
+            categories: ['users', 'clients']
         },
         {
-            icon:'people',
+            icon: 'people',
             label: 'Users',
             route: 'users',
-            categories:['complaints', 'requests']
+            categories: ['complaints', 'requests']
         },
     ])
 
-    collapsed= signal(false);
+    collapsed = signal(false);
     opened = signal(false);
 
-    sidenavWidth = computed(()=> this.collapsed()? '65px' : '250px')
+    sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px')
+
+    profilePicSize = computed(() => this.collapsed() ? '32' : '100')
     
-    profilePicSize = computed(()=> this.collapsed()? '32' : '100')
+    ngOnInit(): void {
+        this.switchTheme.valueChanges.subscribe((currentMode) => {
+            this.className = currentMode ? this.darkClass : this.lightClass
+
+            if (currentMode) {
+                this.overlay.getContainerElement().classList.add(this.darkClass)
+            } else {
+                this.overlay.getContainerElement().classList.remove(this.lightClass)
+            }
+        })
+    }
 }
 
-export type MenuItem ={
-    icon:string;
-    label:string;
-    route?:string;
-    index?:string;
+export type MenuItem = {
+    icon: string;
+    label: string;
+    route?: string;
+    index?: string;
     categories?: string[]
 }
