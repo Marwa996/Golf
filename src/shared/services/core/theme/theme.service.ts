7b9f9e4ca9564bfdd236aa86c9ac5ae57f1ed63c
Marwa996 <m.abdelmoneim@tildetech.ae>
2025-10-01 12:09:17 +0300

    Fixed some issues

--- src/shared/services/core/theme/theme.service.ts
+++ src/shared/services/core/theme/theme.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DARK_THEME_CLASS, LIGHT_THEME_CLASS, THEME_KEY, ThemeType } from '../../../data';
-import { LocalStorageService } from './local-storage/local-storage.service';
+import { LocalStorageService } from '../local-storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root',
